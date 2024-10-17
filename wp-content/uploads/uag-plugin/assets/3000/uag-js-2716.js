a79710ed48beca5b88b5652c1da6a717efcc27d5
Lutfiya Ainurrahman Prasetyo <lutfiyapr.stu@pnc.ac.id>
2024-10-17 17:26:29 +0700

    post1

---
+++ wp-content/uploads/uag-plugin/assets/3000/uag-js-2716.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-ba688ae6' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-18fb1cc9' );
+});
+ });
---
+++ wp-content/uploads/uag-plugin/assets/3000/uag-js-3031.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-ilfqjyhm' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-f5fkfqje' );
+});
+ });
